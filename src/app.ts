b2119b02974234c4d99b6c716c8c4ba2f3465180
Nathakorn Chaninthanadecha <nathakorn.tonch@gmail.com>
2024-02-03 11:54:23 +0700

    Add deps and setup app.ts

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express, { type Express, type Request, type Response } from "express"
+import 'dotenv/config'
+
+const ENV: NodeJS.ProcessEnv = process.env
+const app: Express = express()
+const port = ENV?.PORT || "3000"
+
+app.get('/', (_req: Request, res: Response) => {
+  res.send('Hello World')
+})
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`)
+})
