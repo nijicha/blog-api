888c844adb0be62dc5c390e4e80ed2ea40ad8b10
Nathakorn Chaninthanadecha <nathakorn.tonch@gmail.com>
2024-02-04 21:12:41 +0700

    Change connection.ts from module to class

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,26 +1,28 @@
 import { MongoClient, type Db } from 'mongodb'
 
-async function connection() {
-  const uri: string = process.env.MONGODB_URI || ''
-  const client: MongoClient = new MongoClient(uri)
+export default class DatabaseConnection {
+  private readonly url: string
+  private client: MongoClient
 
-  return {
-    connect: async (): Promise<Db> => {
-      try {
-        await client.connect()
-        return client.db(process.env.DATABASE_NAME)
-      } catch (error) {
-        throw new Error(`Error connecting to the database: ${error}`)
-      }
-    },
-    disconnect: async (): Promise<void> => {
-      try {
-        await client.close()
-      } catch (error) {
-        throw new Error(`Error disconnecting from the database: ${error}`)
-      }
+  constructor() {
+    this.url = process.env.MONGODB_URI || ''
+    this.client = new MongoClient(this.url)
+  }
+
+  public async connect(): Promise<Db> {
+    try {
+      await this.client.connect()
+      return this.client.db(process.env.DATABASE_NAME)
+    } catch (error) {
+      throw new Error(`Error connecting to the database: ${error}`)
+    }
+  }
+
+  public async disconnect(): Promise<void> {
+    try {
+      await this.client.close()
+    } catch (error) {
+      throw new Error(`Error disconnecting from the database: ${error}`)
     }
   }
 }
-
-export default connection
