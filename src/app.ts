4939cb1e21ad5e4bf181d7bf6778c4fbaa520607
Nathakorn Chaninthanadecha <nathakorn.tonch@gmail.com>
2024-02-04 23:53:13 +0700

    Fix dynamic routes and cleancode

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express, { type Express, type Request, type Response } from 'express'
 import cors from 'cors'
+import { readdirSync } from 'fs'
+import { join } from 'path'
+
 import 'dotenv/config'
 import 'module-alias/register'
 
 import CONFIG from './config'
-import articlesRoute from './routes/articles.route'
 
 const ENV: NodeJS.ProcessEnv = process.env
 const app: Express = express()
@@ -13,38 +15,24 @@ const port = ENV?.PORT || '3000'
 app.use(cors(CONFIG.CORS.setting))
 app.use(express.json())
 
-app.use('/articles', articlesRoute)
-
-// FIXME: This is not working
 // Dynamically import route files
+const routesDir = join(__dirname, 'routes')
+readdirSync(routesDir).forEach(async (filename: string) => {
+  const route = join(routesDir, filename)
 
-// const routesDir = join(__dirname, 'routes')
-// readdirSync(routesDir).forEach((filename: string) => {
-//   if (filename.endsWith('.route.ts')) {
-//     import(join(routesDir, filename))
-//       .then(({ default: route }: { default: Router }) => {
-//         const routePath = `/${filename.replace(/\.ts$/, '')}`
-//         app.use(routePath, route)
-//       })
-//       .catch((error) => {
-//         console.error(`Error loading route from ${filename}:`, error)
-//       })
-//   }
-// })
+  try {
+    const item = await import(route)
+    const routeNamespace = `/${filename.replace(/\.route.ts$/, '')}`
+
+    app.use(routeNamespace, item.default)
+  } catch (error) {
+    console.error(`Error loading route from ${filename}:`, error)
+  }
+})
 
 app.get('/', (_req: Request, res: Response) => {
   res.send('Simple Blog API')
 })
-
-// Handle middleware errors
-// app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-//   if (ENV?.NODE_ENV === 'development') {
-//     console.error(err.stack)
-//   }
-//
-//   res.status(500).send('Something went wrong')
-//   next()
-// })
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`)
